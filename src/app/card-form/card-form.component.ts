d37e1d96e170cffbbd014ce7fa1c7b85f10be93a
thanadilok-11576 <thanadilok@aqua.co.th>
2024-02-20 20:00:15 +0700

    prepare for upgrad angular version 13 to 14

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css'],
+})
+export class CardFormComponent implements OnInit {
+  cardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(5),
+      // Validators.pattern(/\s/) // แอดได้เฉพาะ space
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      /** 0[1-9] คือ 01, 09, ...
+       *  1[1-2] คือ 11, 12
+       *  \d{2} คือ เลขสองหลัก
+      */
+      Validators.pattern(/^(0[1-9]|1[1-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ])
+  });
+
+  constructor() {
+    console.log(this.cardForm.get('name'));
+  }
+
+  ngOnInit(): void {}
+
+  getFormValidatorError() {
+    // Object.keys(this.cardForm.controls).forEach((key) => {
+    //   const controlError: any = this.cardForm.get(key)?.errors;
+    //   if (controlError != null) {
+    //     Object.keys(controlError).forEach((keyError) => {
+    //       console.info(
+    //         `💥 Key control: ${key}, keyError: ${keyError}, err value: `,
+    //         controlError[keyError]
+    //       );
+    //     });
+    //   }
+    // });
+  }
+
+  onSubmit() {
+
+  }
+
+  onReset() {
+    this.cardForm.reset();
+  }
+}
---
+++ src/app/date-form-control.ts
@@ -0,0 +1,80 @@
+import { FormControl } from '@angular/forms';
+
+export class DateFormControl extends FormControl {
+  override setValue(value: string, options: any) {
+    console.log('this.value.length: ', this.value.length);
+    console.log(value.length);
+
+    /** /[^0-9|\/]/gi คือ 1-9 กับ / */
+    if (value.match(/[^0-9|\/]/gi)) {
+      /**
+       * value is just function argument and this.value is property of FormControl class instance
+       * ความแตกต่างที่ต้องใส่ this.value เพราะว่าหาก user พิมพ์ 2[หรืออะไรก็ตามที่ match กับ regex] แล้วตามด้วยตัวอักษร[หรืออะไรก็ตามที่ไม่ match กับ regex] มันจะเอาค่าล่าสุดที่ถูกต้องตาม regex มาใส่เป็นค่าของ formControl นั้นก็คือ 2
+       *
+       * */
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length > 5) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    /** handle user can delete / */
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length === 2) {
+      super.setValue(value + '/', { ...options, emitModelToViewChange: true });
+      return;
+    }
+    return super.setValue(value, { ...options, emitModelToViewChange: true });
+  }
+}
+
+// import { FormControl } from "@angular/forms";
+
+// export class DateFormControl extends FormControl {
+
+//   override setValue(value: string | null, options: any) {
+//     if (!value) {
+//       super.setValue('', { ...options, emitModelToViewChange: true });
+//       return;
+//     }
+
+//     /** /[^0-9|\/]/gi คือ 1-9 กับ / */
+//     if (value.match(/[^0-9|\/]/gi)) {
+//       console.log(value.match(/[^0-9|\/]/gi))
+//       console.log('this.value -> ', this.value)
+//       console.log('value -> ', value)
+
+//       super.setValue(this.value, { ...options, emitModelToViewChange: true });
+//       return;
+//     }
+
+//     if (value.length > 5) {
+//       super.setValue(this.value, { ...options, emitModelToViewChange: true });
+//       return;
+//     }
+
+//     /**
+//      * this.value.length === 3 คือ 12/
+//      * this.value คือ ค่าที่เก็บไว้ (this.cardFrom.get('expiration').value)
+//      * value คือ ค่าที่ input เข้ามา realtime
+//      */
+//     if (value.length === 2 && this.value.length === 3) {
+//       super.setValue(value, { ...options, emitModelToViewChange: true });
+//       return;
+//     }
+
+//     if (value.length === 2) {
+//       super.setValue(value + '/', { ...options, emitModelToViewChange: true });
+//       return;
+//     }
+
+//     super.setValue(value, options);
+//   }
+// }
